b48d5aab48eb20e70d7865762d57060cf894cb46
vishalre <vishalgurrala21@gmail.com>
2019-04-20 21:28:01 +0530

    updated

--- insurance/loginapp/models.py
+++ insurance/loginapp/models.py
@@ -2,26 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# printf(timezone.now)
-# Create your models here.
-# TITLE_CHOICES = (
-#     ('Swift', 'Swift'),
-#     ('Ertiga', 'Ertiga'),
-#     ('Baleno', 'Baleno'),
-#     ('Breeza','Breeza'),
-#     ('Ciaz','Ciaz')
-# )
-# AGE=(
-# ('Under 1','Under 1'),
-# ('1 to 2','1 to 2'),
-# ('2 to 3','2 to 3'),
-# ('3 to 4','3 to 4'),
-# ('Over 4','Over 4')
-# )
+
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete = 'cascade')
     license = models.IntegerField(default = 1231)
+    licenseplate = models.CharField(max_length = 50,default = 'licenseplate')
+    profession = models.CharField(max_length = 100, default = 'job')
     address = models.CharField(max_length = 100, blank = True, default = 'place')
     city = models.CharField(max_length = 10, blank = True, default = 'place')
     country = models.CharField(max_length = 10, blank = True, default = 'place')
@@ -33,6 +20,7 @@ class UserProfile(models.Model):
 class detailsmodel(models.Model):
     userid = models.ForeignKey(User, on_delete= 'cascade')
     licnum = models.CharField(max_length=50)
+    lplate = models.CharField(max_length = 50,default = 'lnum')
     carImg = models.ImageField(upload_to='images/')
     speed=models.IntegerField()
     vechilemodel= models.CharField(max_length=10)
--- insurance/populate.py
+++ insurance/populate.py
@@ -20,19 +20,32 @@ fakegen = Faker()
 #         t = User.objects.get_or_create(username = fake_username,first_name = fake_firstname,last_name = fake_lastname,email = fake_email)
 #         # print(fake_username,fake_firstname,fake_lastname)
 
+# def popu_user():
+#     t = User.objects.all()
+#     for i in range(len(t)):
+#         fake_city = fakegen.city()
+#         fake_country = fakegen.country()
+#         fake_addr = fakegen.address()
+#         fake_zip = fakegen.zipcode()
+#         up = UserProfile.objects.get(user = t[i])
+#         up.city = fake_city
+#         up.address = fake_addr
+#         up.country = fake_country
+#         up.zipcode = fake_zip
+#         up.save()
+#         print(i)
+
 def popu_user():
     t = User.objects.all()
     for i in range(len(t)):
-        fake_city = fakegen.city()
-        fake_country = fakegen.country()
-        fake_addr = fakegen.address()
-        fake_zip = fakegen.zipcode()
+        fake_job = fakegen.job()
+        fake_plate = fakegen.license_plate()
+
+        # fake_plate = fakegen.address()
         up = UserProfile.objects.get(user = t[i])
-        up.city = fake_city
-        up.address = fake_addr
-        up.country = fake_country
-        up.zipcode = fake_zip
+        up.licenseplate = fake_plate
+        # up.profession = fake_job
         up.save()
-        print(i)   
+        print(i)
 
 popu_user()
